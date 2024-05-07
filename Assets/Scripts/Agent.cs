cce5bb5758b33d48ba3ea7e98ae6f0d764a5397b
WoutDeBeuckeleer <woutdebeuckeleer@gmail.com>
2024-05-07 16:43:46 +0200

    Added fixes for agent

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -6,15 +6,26 @@ using Unity.MLAgents.Actuators;
 public class MyAgent : Agent
 {
     public Transform target;
-    public float moveSpeed = 1f;
+    public float moveSpeed = 7f;
+    public float turnSpeed = 10f;
+    public LayerMask obstacleLayer;
+    public float raycastDistance = 1f;
     public Transform[] spawnPoints;
 
+    public float distanceCheck = 5f;
+
     private int currentSpawnPointIndex;
+    private Rigidbody rb;
+    public override void Initialize()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     public override void OnEpisodeBegin()
     {
-        currentSpawnPointIndex = Random.Range(0, spawnPoints.Length);
+        // currentSpawnPointIndex = Random.Range(0, spawnPoints.Length);
         transform.position = new Vector3(14f, 3f, -13.5f);
-        ResetTarget();
+        
+        // ResetTarget();
     }
     void ResetTarget()
     {
@@ -31,27 +42,59 @@ public class MyAgent : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        
         float moveX = actions.ContinuousActions[0];
-        float moveZ = actions.ContinuousActions[1];
-        Vector3 move = new Vector3(moveX, 0f, moveZ);
-        transform.Translate(move * moveSpeed * Time.deltaTime);
+        float rotateY = actions.ContinuousActions[1];
+
+        // Move the agent forward in the direction it is facing
+        Vector3 move = transform.forward * moveX * moveSpeed;
+        rb.velocity = move;
+
+        // Draai de agent
+        Vector3 rotation = new Vector3(0f, rotateY, 0f);
+        Quaternion deltaRotation = Quaternion.Euler(rotation * turnSpeed * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+
+        float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+        if (distanceToTarget < distanceCheck)
+        {
+            Debug.Log("In de buurt");
+            SetReward(0.1f);
+        } else
+        {
+            SetReward(-0.05f);
+        }
+
+        
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         var continiousActionsOut = actionsOut.ContinuousActions;
-        continiousActionsOut[0] = Input.GetAxisRaw("Horizontal");
-        continiousActionsOut[1] = Input.GetAxisRaw("Vertical");
+        continiousActionsOut[1] = Input.GetAxisRaw("Horizontal");
+        continiousActionsOut[0] = Input.GetAxisRaw("Vertical");
     }
+    private void FixedUpdate()
+    {
+        // Voer raycasts uit om obstakels te detecteren
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, raycastDistance, obstacleLayer))
+        {
+            // Als de raycast een obstakel raakt, pas dan de bewegingsrichting aan
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, Vector3.Reflect(transform.forward, hit.normal), Mathf.PI, 0f);
+            rb.velocity = newDirection * moveSpeed;
+        }
 
+        // Reset rotation in the x and z axes
+        Quaternion newRotation = Quaternion.Euler(0f, rb.rotation.eulerAngles.y, 0f);
+        rb.MoveRotation(newRotation);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Target")) {
             SetReward(1f);
             Debug.Log("Reward 1");
             EndEpisode();
-            ResetTarget();
+           // ResetTarget();
         }
     }
 }
