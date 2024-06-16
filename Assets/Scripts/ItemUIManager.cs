6c5352c324f518f90d42bebb61d78f59e0e56275
TaneeKempenaers <taneekempenaers@hotmail.com>
2024-06-16 13:47:26 +0200

    fix view and script to show 4 items

--- Assets/Scripts/ItemUIManager.cs
+++ Assets/Scripts/ItemUIManager.cs
@@ -1,13 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ItemUIManager : MonoBehaviour
 {
     public GameObject panel;
-    public GameObject itemUIPrefab; // Prefab voor een enkele rij met item
-    public List<Sprite> itemIcons; // Voeg hier de itemafbeeldingen toe
-    public List<string> itemNames = new List<string> { "Shoe", "Key", "Book", "Medkit" }; // Voeg hier de itemnamen toe
+    public GameObject itemUIPrefab;
+    public List<Texture2D> itemTextures;
+    public List<string> itemNames = new List<string> { "Key", "Books", "Shoe", "Aidkit" };
     private Dictionary<string, Image> itemCheckmarks = new Dictionary<string, Image>();
 
     void Start()
@@ -17,19 +18,33 @@ public class ItemUIManager : MonoBehaviour
 
     void InitializeUI()
     {
+        Debug.Log("Initializing UI...");
+
+        float yOffset = 0f;
+
         for (int i = 0; i < itemNames.Count; i++)
         {
             GameObject itemUI = Instantiate(itemUIPrefab, panel.transform);
+
+            itemUI.transform.localPosition += new Vector3(100f, -yOffset, 0f);
+
             Image icon = itemUI.transform.Find("Icon").GetComponent<Image>();
-            Text nameText = itemUI.transform.Find("Name").GetComponent<Text>();
+            TextMeshProUGUI nameText = itemUI.transform.Find("Name").GetComponent<TextMeshProUGUI>();
             Image checkmark = itemUI.transform.Find("Checkmark").GetComponent<Image>();
 
-            icon.sprite = itemIcons[i];
+            icon.sprite = SpriteFromTexture(itemTextures[i]);
             nameText.text = itemNames[i];
-            checkmark.enabled = true; // Standaard aan, je kunt dit wijzigen naar false indien gewenst
+            checkmark.enabled = true;
 
             itemCheckmarks.Add(itemNames[i], checkmark);
+
+            yOffset += 300f;
         }
+    }
+
+    private Sprite SpriteFromTexture(Texture2D texture)
+    {
+        return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
     }
 
     public void UpdateItem(string itemName, bool collected)
@@ -37,6 +52,11 @@ public class ItemUIManager : MonoBehaviour
         if (itemCheckmarks.ContainsKey(itemName))
         {
             itemCheckmarks[itemName].enabled = collected;
+            Debug.Log($"Item {itemName} updated. Collected: {collected}");
+        }
+        else
+        {
+            Debug.LogWarning($"Item {itemName} not found in dictionary.");
         }
     }
 }
